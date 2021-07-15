1f19e115473cfb786e9325abf645c6369c1cc98a
Michael Kenyi <Michael kenyi>
2021-07-15 15:12:34 +0600

    first commit

---
+++ .history/squirrel_20210715140546.js
@@ -0,0 +1,11 @@
+let day1 = {
+  squirrel: false,
+  events: ["work", "touched tree", "pizza", "running"],
+};
+console.log(day1.squirrel);
+
+console.log(day1.wolf);
+
+day1.wolf = false;
+console.log(day1.wolf);
+console.log(day1.events);
